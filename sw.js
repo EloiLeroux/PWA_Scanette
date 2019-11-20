b94e91d2b4ee51df36ab8dd7c1d1ff74253f965f
Eloi Leroux <eloi.leroux1802@gmail.com>
2019-11-20 16:00:10 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -3,27 +3,30 @@ var cacheName = 'PWA_Scan-v1';
 var appShellFiles = [
   './index.html',
   './manifest.json',
-  './produit.csv',
-  './style.css',
+  './produits.csv',
   './favicon.ico',
+  './js/app.js',
+  './js/DecoderWorker.js',
+  './js/exif.js',
+  './js/job.js',
+  './style.css',
   './images/barcode-scanner.png',
   './images/icon-cart.png',
   './images/icon-setup.png',
   './images/icon-transmit.png',
   './images/logo.png',
-  './js/DecoderWorker.js',
-  './js/app.js',
-  './js/exif.js',
-  './js/job.js',
   './icons/icon-32.png',
   './icons/icon-64.png',
   './icons/icon-96.png',
   './icons/icon-128.png',
   './icons/icon-168.png',
+  './icons/icon-180.png',
   './icons/icon-192.png',
   './icons/icon-256.png',
   './icons/icon-512.png'
 ];
+
+
 
 var contentToCache = appShellFiles;
 
