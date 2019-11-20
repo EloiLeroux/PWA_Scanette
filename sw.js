2da77a004b68ba302960fd1800eb4525f2eb2c92
Eloi Leroux <eloi.leroux1802@gmail.com>
2019-11-20 15:55:46 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -32,9 +32,10 @@ self.addEventListener('install', (e) => {
   e.waitUntil(
     caches.open(cacheName).then((cache) => {
           console.log('[Service Worker] Caching all: app shell and content');
-      return cache.addAll(contentToCache);
+          var a = cache.addAll(contentToCache);
+          console.log('[Service Worker] Caching all: End');
+      return a;
     })
-    console.log('[Service Worker] Testing!!!');
   ); 
 });
 
